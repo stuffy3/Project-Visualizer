c17a5daf595a3f42621b2b132d5ea21462427409
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-11-23 23:36:54 -0700

    finished visualizer

--- Mic visualizer/visualizer.js
+++ Mic visualizer/visualizer.js
@@ -1,9 +1,11 @@
+
+
+
 function main(){
-    console.log('loaded')
     const canvas = document.getElementById("my-canvas")
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight; 
+    canvas.width = window.innerWidth/1;
+    canvas.height = window.innerHeight/1.15; 
 
     class Bar {
         constructor(x, y, width, height, color, index) {
@@ -15,72 +17,137 @@ function main(){
             this.index = index;
     
         }
-        update(micInput){ //this is where we pass in the audio input to generate the bars
-            const sound = micInput * 10000
+        update(micInput, scale){ //this is where we pass in the audio input to generate the bars
+            const sound = micInput * 3000
             if(sound > this.height){
                 this.height = sound;
             }else{
-               this.height -= this.height * 0.03
+               this.height -= this.height * .05
             }
             
         }
-        draw(context, volume) {//draw shapes based on values
-            context.strokeStyle = this.color;
+        draw(context, volume, scale, color) {//draw shapes based on values
+            context.strokeStyle = color;
             context.save();
             
             context.translate(0, 0);
             context.rotate(this.index * 1);
-            context.scale(1 + volume * 0.2, 1 + volume * 0.2)
+            context.scale(scale, scale)
+            
         //    context.fillRect(this.x, this.y, this.width, this.height)
             context.beginPath();
             context.moveTo(this.x/2, this.y/2);
             context.lineTo(this.y, this.height/3);
-           
+            
             context.stroke();
             
             context.restore();
-    
+            
         }
-    
+
+        drawBars(context){
+            context.strokeStyle = this.color;
+            context.save();
+            context.beginPath();
+            context.moveTo(0, 0);
+            context.lineTo(this.x, this.y);
+            context.stroke();
+            context.closePath()
+        }
+        
     }
     
-    const microphone = new Microphone();
+    
+    
+    
+    
+    //    window.location.reload()
+    var slider2 = document.getElementById("myRange2").value;
+    const fftSize = 512
+    
+    const microphone = new Microphone(fftSize);
+    
     let bars = []
-    let barWidth = canvas.width/256
+    let barWidth = canvas.width/(slider2)
+    
     function createBars(){
-        for(let i = 0; i < 256; i++){
-            let color = 'hsl(0, 100%, 100%)'
-            bars.push(new Bar(0, i*2, 2, 1, color, i))
+        let value = document.getElementById("myRange2").value
+        let slider3 = document.getElementById("myRange3").value;
+        
+        for(let i = 0; i < `${value}`/2; i++){
+            let color = `hsl(${slider3}, 100%, 50% )`
+            bars.push(new Bar(0, i*2, barWidth, 1, color, i))
+            
         }
     }
+    
+    // let sliderEle3 = document.getElementById("myRange3")
+    // sliderEle3.onchange = function(){
+    // window.location.reload()
+    // }
+    let sliderEle = document.getElementById("myRange2")
+    sliderEle.onchange  = function(){
+        window.location.reload()
+    }
+    
+    
     createBars();
     console.log(bars)
     let angle = 0;
-
-
+    
+    
     function animate(){
+        var slider = document.getElementById("myRange").value;
+        var slider3 = document.getElementById("myRange3").value
         if(microphone.initialized){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
-       
-        //generates audio sample from microphone
-        const samples = microphone.getSamples()
-        const volume = microphone.getVolume()
-        angle += 0.001;
-        ctx.save()
-        ctx.translate(canvas.width/2, canvas.height/2)
-        ctx.rotate(angle)
-        //animate bars based on microphone data
-        bars.forEach(function(bar, i){
-            bar.update(samples[i]);
-            bar.draw(ctx);
-            bar.draw(ctx, volume)
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             
-        });
-        ctx.restore()
-    }
+            
+            
+            //generates audio sample from microphone
+            const samples = microphone.getSamples()
+            const volume = microphone.getVolume()
+            const scale = slider
+            const color = `hsl(${slider3}, 100%, 50% )`
+            angle += 0.001;
+            ctx.save()
+            ctx.translate(canvas.width/2, canvas.height/2)
+            ctx.rotate(angle)
+            //animate bars based on microphone data
+            bars.forEach(function(bar, i){
+                
+                bar.update(samples[i], scale*2);
+                bar.draw(ctx, volume/4, scale/2, color)
+                
+            });
+            ctx.restore()
+            
+        }
         requestAnimationFrame(animate);
     }
     
     animate();
+    
 }
+
+
+
+
+// Update the current slider value (each time you drag the slider handle)
+
+
+// Display the default slider value
+// function drawParticles() {
+    //     // PARTICLES //
+    
+    //     context.beginPath();
+    //     for (i=0; i<particles.length; i++) {
+        //         var p = particles[i];
+        //         if (p.active) {
+            //             context.moveTo(p.position.x, p.position.y - (p.size*units));
+            //             context.lineTo(p.position.x, p.position.y + (p.size*units));
+            //         }
+//     }
+//     context.stroke();
+// }
+// requestAnimationFrame(drawParticles);
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const path = require('path')
+const cors = require("cors")
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
+    
+});
+
+app.get('/visualizer.js', function(req, res) {
+    res.sendFile(path.join(__dirname, '../visualizer.js'))
+    
+});
+
+app.get('/mic.js', function(req, res) {
+    res.sendFile(path.join(__dirname, '../mic.js'))
+    
+});
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Docked at port ${port}`)
+})
